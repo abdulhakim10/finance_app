ccea51a23e6206a5fd5d0ad125dd9954a5b5ec80
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-08-15 16:11:00 +0600

    date and save button added

--- lib/Screens/add.dart
+++ lib/Screens/add.dart
@@ -10,17 +10,20 @@ class Add_Screen extends StatefulWidget {
 }
 
 class _Add_ScreenState extends State<Add_Screen> {
+  DateTime date = DateTime.now();
   final TextEditingController expalin_C = TextEditingController();
   FocusNode ex = FocusNode();
   final TextEditingController amount_c = TextEditingController();
   FocusNode amount_ = FocusNode();
   String? selectedItem;
+  String? selectedItemi;
   final List<String> _item = [
     'Food',
     'Transfer',
     'Transportation',
     'Education'
   ];
+  final List<String> _itemi = ['Income', 'Expend'];
   @override
   void initState() {
     // TODO: implement initState
@@ -67,36 +70,163 @@ class _Add_ScreenState extends State<Add_Screen> {
           const SizedBox(height: 30),
           explain(),
           const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              focusNode: amount_,
-              controller: amount_c,
-              decoration: InputDecoration(
-                contentPadding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                labelText: 'amount',
-                labelStyle:
-                    TextStyle(fontSize: 17, color: Colors.grey.shade500),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(
-                    width: 2,
-                    color: Color(0xffC5C5C5),
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(
-                    width: 2,
-                    color: Color(0xff368983),
-                  ),
-                ),
-              ),
+          amount(),
+          const SizedBox(height: 30),
+          how(),
+          const SizedBox(height: 30),
+          date_time(),
+          const Spacer(),
+          save(),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  GestureDetector save() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: 120,
+        height: 50,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: const Color(0xff368983),
+        ),
+        child: const Text(
+          'Save',
+          style: TextStyle(
+              fontFamily: 'f',
+              fontSize: 17,
+              fontWeight: FontWeight.w700,
+              color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget date_time() {
+    return Container(
+      alignment: Alignment.bottomLeft,
+      width: 300,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(width: 2, color: const Color(0xffC5C5C5))),
+      child: TextButton(
+          onPressed: () async {
+            DateTime? newDate = await showDatePicker(
+                context: context,
+                initialDate: date,
+                firstDate: DateTime(2020),
+                lastDate: DateTime(2100));
+            if (newDate == Null) return;
+            setState(() {
+              date = newDate!;
+            });
+          },
+          child: Text(
+            'date: ${date.year} / ${date.day} / ${date.month}',
+            style: const TextStyle(fontSize: 15, color: Colors.black),
+          )),
+    );
+  }
+
+  Padding how() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        width: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            width: 2,
+            color: const Color(0xffC5C5C5),
+          ),
+        ),
+        child: DropdownButton<String>(
+            value: selectedItemi,
+            items: _itemi
+                .map((e) => DropdownMenuItem(
+                      value: e,
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 40,
+                              child: Image.asset('assets/images/$e.png'),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              e,
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ))
+                .toList(),
+            selectedItemBuilder: (context) => _itemi
+                .map((e) => Row(
+                      children: [
+                        SizedBox(
+                          width: 40,
+                          child: Image.asset('assets/images/$e.png'),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(e)
+                      ],
+                    ))
+                .toList(),
+            hint: const Text(
+              'How',
+              style: TextStyle(color: Colors.grey),
+            ),
+            dropdownColor: Colors.white,
+            isExpanded: true,
+            underline: Container(),
+            onChanged: (value) {
+              setState(() {
+                selectedItemi = value!;
+              });
+            }),
+      ),
+    );
+  }
+
+  Padding amount() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: TextField(
+        keyboardType: TextInputType.number,
+        focusNode: amount_,
+        controller: amount_c,
+        decoration: InputDecoration(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          labelText: 'amount',
+          labelStyle: TextStyle(fontSize: 17, color: Colors.grey.shade500),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              width: 2,
+              color: Color(0xffC5C5C5),
             ),
           ),
-        ],
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(
+              width: 2,
+              color: Color(0xff368983),
+            ),
+          ),
+        ),
       ),
     );
   }
